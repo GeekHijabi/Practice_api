4b15a5317f20a937897589887f1edfd30355edb6
GeekHijabi <bimpeadetunji@rocketmail.com>
2017-08-17 12:01:13 +0100

    API trial

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask_jsonpify import jsonify
-
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
@@ -31,6 +29,7 @@ class Employees_Name(Resource):
         query = conn.execute('select * from employees where Employeeid = %d ' %int(employee_id))
         result = {'data':[dict(zip(tuple(query.keys()) ,i))for i in query.cursor]}
         return jsonify(result)
+
 
 api.add_resource(Employees, '/employees')
 api.add_resource(Tracks, '/tracks')
